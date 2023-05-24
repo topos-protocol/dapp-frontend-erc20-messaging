1cf82a9baf1104799f5926fefc65a9674634d5d9
Sébastien Dan <sebastien.dan@gmail.com>
2023-05-24 18:06:09 -0400

    chore: clean a few things

--- packages/frontend/cypress/e2e/wallet-connect.cy.ts
+++ packages/frontend/cypress/e2e/wallet-connect.cy.ts
@@ -3,19 +3,44 @@ describe('connect wallet spec', () => {
     cy.visit('/')
   })
 
-  afterEach(() => {
+  before(() => {
+    cy.visit('/')
+    cy.get('#connectButton').click()
+    cy.acceptMetamaskAccess()
+  })
+
+  after(() => {
     cy.disconnectMetamaskWalletFromAllDapps()
     cy.resetMetamaskAccount()
   })
 
   it('should connect wallet with success', () => {
-    cy.get('#connectButton').click()
-    cy.acceptMetamaskAccess()
     cy.get('#account').should(
       'have.text',
       // Set by PRIVATE_KEY in env which synpress uses to import the account in general before() init function call
       // This address is the Topos Deployer address
       '0x4aab25b4fad0beaac466050f3a7142a502f4cf0a'
     )
+  })
+
+  it('should list Topos, Incal, and Edena', () => {
+    cy.get('#sendingSubnet').click()
+    cy.get('.ant-select-item-option-content').contains('Topos').should('exist')
+    cy.get('.ant-select-item-option-content').contains('Incal').should('exist')
+    cy.get('.ant-select-item-option-content').contains('Edena').should('exist')
+  })
+
+  it('should have all three visible on click', () => {
+    cy.get('#sendingSubnet').click()
+
+    cy.get('.ant-select-item-option-content')
+      .contains('Topos')
+      .should('be.visible')
+    cy.get('.ant-select-item-option-content')
+      .contains('Incal')
+      .should('be.visible')
+    cy.get('.ant-select-item-option-content')
+      .contains('Edena')
+      .should('be.visible')
   })
 })
