756ae569b9c85af2073771ae81720c30a7a5ce5d
Sébastien Dan <sebastien.dan@gmail.com>
2023-06-07 12:17:09 -0400

    fix: fix

--- packages/frontend/cypress/e2e/step1.cy.ts
+++ packages/frontend/cypress/e2e/step1.cy.ts
@@ -13,7 +13,7 @@ describe('Multistep form step-1 with Topos', () => {
     cy.acceptMetamaskAccess()
     cy.get('#sendingSubnet').click()
     cy.get('.ant-select-item-option-content').contains('Topos').click()
-    cy.allowMetamaskToAddAndSwitchNetwork()
+    cy.allowMetamaskToSwitchNetwork()
     cy.get('#nextButton').click()
   })
 
