23523de6f96ba51df07158ba1354a2a740b3098d
Sébastien Dan <sebastien.dan@gmail.com>
2023-08-02 11:02:25 +0200

    test: add backend unit tests

--- packages/backend/src/auth/auth.service.spec.ts
+++ packages/backend/src/auth/auth.service.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { ConfigService } from '@nestjs/config'
+import axios from 'axios'
+
 import { BackendAuthService } from './auth.service'
+
+const config = {
+  AUTH0_AUDIENCE: 'audience',
+  AUTH0_CLIENT_ID: 'client id',
+  AUTH0_CLIENT_SECRET: 'client secret',
+  AUTH0_ISSUER_URL: 'issue url',
+}
 
 describe('BackendAuthService', () => {
   let service: BackendAuthService
+  let axiosPostMock
+  let axiosCreateSpy
 
   beforeEach(async () => {
+    axiosPostMock = jest.fn().mockResolvedValue({ data: { auth_token: '' } })
+
+    axiosCreateSpy = jest
+      .spyOn<any, any>(axios, 'create')
+      .mockReturnValue({ post: axiosPostMock })
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [BackendAuthService],
-    }).compile()
+    })
+      .useMocker((token) => {
+        if (token === ConfigService) {
+          return {
+            getOrThrow: jest.fn().mockImplementation((key) => config[key]),
+          }
+        }
+      })
+      .compile()
 
     service = module.get<BackendAuthService>(BackendAuthService)
   })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined()
+  describe('getAuthToken', () => {
+    it('should call axios create to create api', () => {
+      service.getAuthToken()
+      expect(axiosCreateSpy).toHaveBeenCalled()
+    })
+
+    it('should call axios post to request auth token', () => {
+      service.getAuthToken()
+      expect(axiosPostMock).toHaveBeenCalledWith('oauth/token', {
+        audience: config[`AUTH0_AUDIENCE`],
+        grant_type: 'client_credentials',
+        client_id: config[`AUTH0_CLIENT_ID`],
+        client_secret: config[`AUTH0_CLIENT_SECRET`],
+      })
+    })
   })
 })
